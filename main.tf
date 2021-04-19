795622a5fb999908ee236a70c1041aad1f65d1f8
faraz891 <8448574+faraz891@users.noreply.github.com>
2021-04-19 14:30:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 # Use AWS Terraform provider
 provider "aws" {
   region = "us-west-2"
-  access_key = "AKIA5MZCKJMC6XAPYEBO"
-  secret_key= "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"
+  access_key = "AKIA5MZCKJMCZNL4DSEE"
+  secret_key= "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"
 }
 
 # Create EC2 instance
